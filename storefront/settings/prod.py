4455dc0f2db71b17212f49c17d458581108e21da
Joe <joe_walsh59@yahoo.com>
2023-07-06 12:31:46 -0500

    Prepare to deploy

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,8 +1,20 @@
 import os
+import dj_database_url
 from .common import *
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['joebuy-prod-593d67cc2d3f.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+
+}
+
+
+EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
+EMAIL_HOST_USER = os.environ['MAILGUN_SMTP_PASSWORD']
+EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
+EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
